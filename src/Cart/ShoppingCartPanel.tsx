5d4953a5e16b21d7d9423265e2e5ee22c1d5260e
seabAu <79596199+seabAu@users.noreply.github.com>
2022-10-29 20:40:43 -0400

    Improvements

--- src/Cart/ShoppingCartPanel.tsx
+++ src/Cart/ShoppingCartPanel.tsx
@@ -7,7 +7,41 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { BrowserRouter as Router, Link } from "react-router-dom";
 
+import React, { useState } from "react";
+
 const ShoppingCartPanel = (props: any): JSX.Element => {
+    const [products, setProducts] = useState([
+        {
+            name: "Product A",
+            description: "Product Description Here",
+            price: 25.0,
+            quantity: 2,
+        },
+        {
+            name: "Product B",
+            description: "Product Description Here",
+            price: 25.0,
+            quantity: 1,
+        },
+    ]);
+
+    function deleteProduct(productIndex: number): void {
+        // products = products.filter((_, index) => index !== productIndex);
+        setProducts(products.filter((_, index) => index !== productIndex));
+    }
+
+    function getTotal (): number
+    {
+        // Run through each product and add up the price.
+        let total: number = 0;
+        products.forEach( ( product ) =>
+            {
+                total += product.price * product.quantity;
+            }
+        );
+        return total;
+    }
+
     return (
         <div className="container">
             <div className="row">
@@ -38,101 +72,67 @@ const ShoppingCartPanel = (props: any): JSX.Element => {
                             </div>
                         </div>
                         <div className="card-body">
-                            <div className="row">
-                                <div className="col-2">
-                                    <img
-                                        className="img-responsive"
-                                        src="http://placehold.it/100x70"
-                                        alt="placeholder"
-                                    />
-                                </div>
-                                <div className="col-4">
-                                    <h4 className="product-name">
-                                        <strong>Product name</strong>
-                                    </h4>
-                                    <h4>
-                                        <small>Product description</small>
-                                    </h4>
-                                </div>
-                                <div className="col-6">
-                                    <div className="row">
-                                        <div className="col-6 text-end">
-                                            <h6>
-                                                <strong>
-                                                    25.00{" "}
-                                                    <span className="text-muted">
-                                                        x
-                                                    </span>
-                                                </strong>
-                                            </h6>
-                                        </div>
-                                        <div className="col-4">
-                                            <input
-                                                type="text"
-                                                className="form-control input-sm"
-                                                value="1"
+                            {products.map((product, productIndex) => {
+                                return (
+                                    <div className="row" key="{productIndex}">
+                                        <div className="col-2">
+                                            <img
+                                                className="img-responsive"
+                                                src="http://placehold.it/100x70"
+                                                alt="placeholder"
                                             />
                                         </div>
-                                        <div className="col-2">
-                                            <button
-                                                type="button"
-                                                className="btn btn-link btn">
-                                                <FontAwesomeIcon
-                                                    icon={faTrash}
-                                                />
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <hr />
-                            <div className="row">
-                                <div className="col-2">
-                                    <img
-                                        className="img-responsive"
-                                        src="http://placehold.it/100x70"
-                                        alt="placeholder"
-                                    />
-                                </div>
-                                <div className="col-4">
-                                    <h4 className="product-name">
-                                        <strong>Product name</strong>
-                                    </h4>
-                                    <h4>
-                                        <small>Product description</small>
-                                    </h4>
-                                </div>
-                                <div className="col-6">
-                                    <div className="row">
-                                        <div className="col-6 text-end">
-                                            <h6>
-                                                <strong>
-                                                    25.00{" "}
-                                                    <span className="text-muted">
-                                                        x
-                                                    </span>
-                                                </strong>
-                                            </h6>
-                                        </div>
                                         <div className="col-4">
-                                            <input
-                                                type="text"
-                                                className="form-control input-sm"
-                                                value="1"
-                                            />
+                                            <h4 className="product-name">
+                                                <strong>{product.name}</strong>
+                                            </h4>
+                                            <h4>
+                                                <small>
+                                                    {product.description}
+                                                </small>
+                                            </h4>
                                         </div>
-                                        <div className="col-2">
-                                            <button
-                                                type="button"
-                                                className="btn btn-link btn">
-                                                <FontAwesomeIcon
-                                                    icon={faTrash}
-                                                />
-                                            </button>
+                                        <div className="col-6">
+                                            <div className="row">
+                                                <div className="col-6 text-end">
+                                                    <h6>
+                                                        <strong>
+                                                            {product.price}{" "}
+                                                            <span className="text-muted">
+                                                                x
+                                                            </span>
+                                                        </strong>
+                                                    </h6>
+                                                </div>
+                                                <div className="col-4">
+                                                    <input
+                                                        type="text"
+                                                        className="form-control input-sm"
+                                                        defaultValue={
+                                                            product.quantity
+                                                        }
+                                                    />
+                                                </div>
+                                                <div className="col-2">
+                                                    <button
+                                                        type="button"
+                                                        className="btn btn-link btn"
+                                                        onClick={() =>
+                                                            deleteProduct(
+                                                                productIndex,
+                                                            )
+                                                        }>
+                                                        <FontAwesomeIcon
+                                                            icon={faTrash}
+                                                        />
+                                                    </button>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
+                                );
+                            })}
+
                             <hr />
                             <div className="row text-center align-items-center">
                                 <div className="col-9">
@@ -143,7 +143,8 @@ const ShoppingCartPanel = (props: any): JSX.Element => {
                                 <div className="col-3">
                                     <button
                                         type="button"
-                                        className="btn btn-default btn-sm w-100 border">
+                                        className="btn btn-default btn-sm w-100 border"
+                                        onClick={() => console.log(products)}>
                                         Update cart
                                     </button>
                                 </div>
@@ -153,7 +154,7 @@ const ShoppingCartPanel = (props: any): JSX.Element => {
                             <div className="row text-center align-items-center">
                                 <div className="col-9">
                                     <h4 className="text-end mb-0 h5">
-                                        Total <strong>$50.00</strong>
+                                        Total <strong>${getTotal()}</strong>
                                     </h4>
                                 </div>
                                 <div className="col-3">
